4af00996a0ba646fd952d6dd7e2fb3813359844f
cmwedin <cmwedin@gmail.com>
2022-05-05 15:16:20 -0400

    added underlying values

--- Runtime/GraphNode.cs
+++ Runtime/GraphNode.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 namespace SadSapphicGames.CustomGraphs {
     public class GraphNode<TGraphType> {
         private int id;
+        private TGraphType value;
         private List<int> neighborIDs; 
         public List<int> NeighborIDs { get => neighborIDs; set => neighborIDs = value; }
         public int ID { get => id; set => id = value; }
+        public TGraphType Value { get => value;}
 
 
         // * Constructor
         public GraphNode(int _id, List<int>_neighborIDs) {
             this.ID = _id;
             this.NeighborIDs = _neighborIDs;
+            this.value = default(TGraphType);
         }
 
     }
