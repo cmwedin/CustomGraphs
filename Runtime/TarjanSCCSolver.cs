34389a05c371aebad151021c059e492fafb93f0a
cmwedin <cmwedin@gmail.com>
2022-05-08 10:51:57 -0400

    finished \ refined Tarjan
    now fully ready for testing

--- Runtime/TarjanSCCSolver.cs
+++ Runtime/TarjanSCCSolver.cs
@@ -5,13 +5,16 @@ using System.Collections.Generic;
 namespace SadSapphicGames.CustomGraphs{
     public class TarjanSCCSolver<TGraphType> {
         private Graph<TGraphType> graph;
-        private bool[] onStack, visited;
-        private int[] low, scc;
-        private Stack<GraphNode<TGraphType>> nodeStack;
+        //? these could also be arrays but im using dicts to decouple the behavior of graph nodes from int id's incase i want to change that in the future
+        //? however the space complexity of this solution could be rather large, testing will be needed
+        private Dictionary<GraphNode<TGraphType>,bool> onStack; //? bools to track if each node is on the stacks or visited
+        private Dictionary<GraphNode<TGraphType>, int> tarjanIDs; //? order of each node in the algorithm, also wether the node is unvisited (-1)
+        private int[] lowlink; //? lowest valued id reachable from the node
+        private Stack<GraphNode<TGraphType>> nodeStack; //? stack of nodes in the current scc
 
         private bool solved = false;
-        List<List<GraphNode<TGraphType>>> solution = new List<List<GraphNode<TGraphType>>>();
-
+        private Dictionary<GraphNode<TGraphType>,int> finalLowLinks = new Dictionary<GraphNode<TGraphType>, int>();
+        private Dictionary<int,List<GraphNode<TGraphType>>> solution = new Dictionary<int, List<GraphNode<TGraphType>>>();
         // * Constructor
         public TarjanSCCSolver(Graph<TGraphType> _graph) {
             if(_graph == null) throw new System.Exception("TarjanSolver's graph cannot be null");
@@ -19,7 +22,7 @@ namespace SadSapphicGames.CustomGraphs{
             this.graph = _graph;
         }
 
-        public List<List<GraphNode<TGraphType>>> GetSolution() {
+        public Dictionary<int,List<GraphNode<TGraphType>>> GetSolution() {
             if(solved) return solution;
             Solve();
             return solution;
@@ -27,40 +30,55 @@ namespace SadSapphicGames.CustomGraphs{
 
         private void Solve() {
             if(solved) return;
-            //ids = new int[graph.Size];
-            low = new int[graph.Size];
-            scc = new int[graph.Size];
-            Array.Fill<int>(scc, -1);
 
-            visited = new bool[graph.Size];
-            onStack = new bool[graph.Size];
+            lowlink = new int[graph.Size];
+
+            foreach (var node in graph.Nodes.Values) { //? this does take O(V) time however at sufficiently large arguments  O(V)+O(V+E) ~ O(V+E)
+                tarjanIDs.Add(node, -1);
+                onStack.Add(node, false);
+            }
+
             nodeStack = new Stack<GraphNode<TGraphType>>();
-
+            int iterationDepth = -1;
             for (int id = 0; id < graph.Size; id++) {
-                if(visited[id] == false) TarjanDFS(graph.Nodes[id]); //? the start of tarjan can be random so we just use whatever node in the graph happens to have id 0
+                if(tarjanIDs[graph.Nodes[id]] == -1) TarjanDFS(graph.Nodes[id],iterationDepth,out iterationDepth); 
+                //? the start of tarjan can be random so we just use whatever node in the graph happens to have id 0
+                //? if i make node id's a generic type in the future I could pick a random element of graph.Nodes.Values
             }
         }
 
-        private void TarjanDFS(GraphNode<TGraphType> node) {
+        private void TarjanDFS(
+            GraphNode<TGraphType> node, //? what node are we starting at?
+            int iterationDepth, //? how deep in a recursive call stack are we, -1 for the initial call
+            out int outDepth //? so we can tell how deep we went on returning to in initial for loop in Solve()
+        ) {
+            iterationDepth++;
+            if(iterationDepth > graph.Size) throw new Exception("iteration depth to large, terminating algorithm");
             nodeStack.Push(node);
-            onStack[node.ID] = true;
-            visited[node.ID] = true;
-            low[node.ID] = node.ID;
+            onStack[node] = true;
+            tarjanIDs[node] = iterationDepth;
+            lowlink[iterationDepth] = iterationDepth;
+
 
             //? for all the edges on the start node, recursively call the function and update the value of low-link value of that node 
             foreach (var edge in node.Edges) {
                 GraphNode<TGraphType> neighbor = edge.GetOppositeNode(node);
-                if(visited[neighbor.ID] == false) {
-                    TarjanDFS(neighbor); } //? call this function for my neighbor
-                if(onStack[neighbor.ID]) low[node.ID] = Math.Min(low[node.ID],low[neighbor.ID]); //? if my neighbor found a lower link than me, update mine to theirs
+                if(tarjanIDs[neighbor] == -1) {
+                    TarjanDFS(neighbor, iterationDepth, out iterationDepth); } //? call this function for my neighbor
+                if(onStack[neighbor]) lowlink[tarjanIDs[node]] = Math.Min(lowlink[tarjanIDs[node]],lowlink[tarjanIDs[neighbor]]); //? if my neighbor found a lower link than me, update mine to theirs
             }
 
-            if(low[node.ID] == node.ID) { //? if this node is the start of a strongly connected component
-                while(nodeStack.TryPop(out GraphNode<TGraphType> topNode) && topNode.ID != node.ID) { //? remove all nodes from the stack till we reach its initial call
-                    onStack[topNode.ID] = false;
-                    low[topNode.ID] = node.ID; //?
+            if(lowlink[tarjanIDs[node]] == tarjanIDs[node]) { //? if this node is the start of a strongly connected component
+                List<GraphNode<TGraphType>> sccList = new List<GraphNode<TGraphType>>();
+                while(nodeStack.TryPop(out GraphNode<TGraphType> topNode)) { //? remove all nodes from the stack till we reach its initial call
+                    onStack[topNode] = false;
+                    sccList.Add(topNode);
+                    finalLowLinks[topNode] = tarjanIDs[node];
+                    if(tarjanIDs[topNode] == tarjanIDs[node]) break;
                 }
+                solution.Add(tarjanIDs[node],sccList);
             }
+            outDepth = iterationDepth;
         }
     }
 }
