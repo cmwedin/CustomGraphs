a1d6db630b4b49a7b4d061834751222b4f6a737e
cmwedin <cmwedin@gmail.com>
2022-05-21 10:26:26 -0400

    more cycle solver testing
    verified undirect graphs work, added new file for cycle related tests

---
+++ Tests/Editor/CyclesTests.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using SadSapphicGames.CustomGraphs;
+
+public class CyclesTests
+{
+    DirectedGraph<bool> trivialGraph = new DirectedGraph<bool>( new Dictionary<int, List<int>> {
+        {0, new List<int>()}
+    });
+    DirectedGraph<bool> trivialCycle = new DirectedGraph<bool>( new Dictionary<int, List<int>> {
+        {0, new List<int>{1}},
+        {1, new List<int>{0}}
+    });
+    DirectedGraph<bool> scc = new DirectedGraph<bool> ( new Dictionary<int, List<int>> { 
+        {0, new List<int>{1}},
+        {1, new List<int>{2}},
+        {2, new List<int>{0}}, //? end of scc 1
+        {3, new List<int>{0,2,4}},
+        {4, new List<int>{5}},
+        {5, new List<int>{0,3}}, //? end of scc 2
+        {6, new List<int>{4,7}},
+        {7, new List<int>{6,5}}, //? end of scc 3
+    });
+    DirectedGraph<bool> childNodes = new DirectedGraph<bool>( new Dictionary<int, List<int>> {
+        {0, new List<int>{1,2}},
+        {1, new List<int>{3}},
+        {2, new List<int>{4}},
+        {3, new List<int>{}},
+        {4, new List<int>{}}
+    });
+
+    UndirectedGraph<bool> undirectedCycle = new UndirectedGraph<bool>( new Dictionary<int, List<int>> {
+        {0, new List<int>{1,2}},
+        {1, new List<int>{2}},
+        {2, new List<int>{}}
+    } );
+    [Test]
+    public void DAGTest() {
+        Assert.IsTrue(TarjanSCCSolver<bool>.CheckDAG(childNodes));
+        Assert.IsFalse(TarjanSCCSolver<bool>.CheckDAG(scc));
+    }
+    [Test]
+    public void TarjanTest() {
+        var tarjanSolution = TarjanSCCSolver<bool>.Solve(scc);
+        Assert.AreEqual(
+            expected: 3,
+            // actual: solution.Values.Count
+            actual: tarjanSolution.Count
+        );
+        Assert.AreEqual(
+            expected: new List<GraphNode<bool>> {scc.GetNode(2),scc.GetNode(1),scc.GetNode(0)},
+            actual: tarjanSolution[0]
+        );
+        Assert.AreEqual(
+            expected: new List<GraphNode<bool>> {scc.GetNode(5),scc.GetNode(4),scc.GetNode(3)},
+            actual: tarjanSolution[1]
+        );
+        Assert.AreEqual(
+            expected: new List<GraphNode<bool>> {scc.GetNode(7),scc.GetNode(6)},
+            actual: tarjanSolution[2]
+        );
+    }
+    [Test]
+    public void CycleSolverTest() {
+        Assert.IsTrue(CycleSolver<bool>.FindCycleFrom(scc.GetNode(1)));
+        Assert.IsTrue(CycleSolver<bool>.FindCycleFrom(scc.GetNode(4)));
+        Assert.IsTrue(CycleSolver<bool>.FindCycleFrom(scc.GetNode(6)));
+        Assert.IsTrue(CycleSolver<bool>.FindCycleFrom(undirectedCycle.GetNode(0)));
+        Assert.IsFalse(CycleSolver<bool>.FindCycleFrom(childNodes.GetNode(0)));
+    }
+}
--- Tests/Editor/GraphTests.cs
+++ Tests/Editor/GraphTests.cs
@@ -28,16 +28,7 @@ public class GraphTests
         {2, new List<int>{3}},
         {3, new List<int>{2}}
     });
-    DirectedGraph<bool> scc = new DirectedGraph<bool> ( new Dictionary<int, List<int>> { 
-        {0, new List<int>{1}},
-        {1, new List<int>{2}},
-        {2, new List<int>{0}}, //? end of scc 1
-        {3, new List<int>{0,2,4}},
-        {4, new List<int>{5}},
-        {5, new List<int>{0,3}}, //? end of scc 2
-        {6, new List<int>{4,7}},
-        {7, new List<int>{6,5}}, //? end of scc 3
-    });
+
     DirectedGraph<bool> bottleNeck = new DirectedGraph<bool> ( new Dictionary<int, List<int>> {
         {0, new List<int>{3}},
         {1, new List<int>{3}},
@@ -111,37 +102,7 @@ public class GraphTests
             expected:false,
             actual:islands.HasPath(0,3));
     }
-    [Test]
-    public void TarjanTest() {
-        var tarjanSolution = TarjanSCCSolver<bool>.Solve(scc);
-        Assert.AreEqual(
-            expected: 3,
-            // actual: solution.Values.Count
-            actual: tarjanSolution.Count
-        );
-        Assert.AreEqual(
-            expected: new List<GraphNode<bool>> {scc.GetNode(2),scc.GetNode(1),scc.GetNode(0)},
-            actual: tarjanSolution[0]
-        );
-        Assert.AreEqual(
-            expected: new List<GraphNode<bool>> {scc.GetNode(5),scc.GetNode(4),scc.GetNode(3)},
-            actual: tarjanSolution[1]
-        );
-        Assert.AreEqual(
-            expected: new List<GraphNode<bool>> {scc.GetNode(7),scc.GetNode(6)},
-            actual: tarjanSolution[2]
-        );
-    }
 
-    [Test]
-    public void DAGTest() {
-        Assert.IsTrue(TarjanSCCSolver<bool>.CheckDAG(childNodes));
-        Assert.IsFalse(TarjanSCCSolver<bool>.CheckDAG(scc));
-        Assert.IsTrue(CycleSolver<bool>.FindCycleFrom(scc.GetNode(1)));
-        Assert.IsTrue(CycleSolver<bool>.FindCycleFrom(scc.GetNode(4)));
-        Assert.IsTrue(CycleSolver<bool>.FindCycleFrom(scc.GetNode(6)));
-        Assert.IsFalse(CycleSolver<bool>.FindCycleFrom(childNodes.GetNode(0)));
-    }
     [Test]
     public void SortTest() {
         var sortedNodes = TopologicalSort<bool>.Sort(unsorted);
