34eba3f4e119543711f18a467942d83e61b51a40
cmwedin <cmwedin@gmail.com>
2022-05-21 11:36:40 -0400

    VerifyTree method
    this method is static so we can use it to verify trees in other contexts as well.

--- Runtime/Graphs/Tree.cs
+++ Runtime/Graphs/Tree.cs
@@ -5,7 +5,9 @@ namespace SadSapphicGames.CustomGraphs {
     public class Tree<TGraphType> : UndirectedGraph<TGraphType>
     {
         public Tree(Dictionary<int, List<int>> adjacencyList) : base(adjacencyList) {
-            
+            //? we shouldn't have to do anything special when constructing a tree
+            //? so we just check this actually is a tree after the base constructor does its job
+            if(!Tree<TGraphType>.VerifyTree(this)) throw new NotATreeException();
         }
 
         public static bool VerifyTree(AbstractGraph<TGraphType> _graph) {
@@ -20,10 +22,16 @@ namespace SadSapphicGames.CustomGraphs {
                 if(visitedNodes.Contains(_node)) { continue;
                 } else if(visitedNodes == null) {
                     //? this is the first node we tested
-                    if(CycleSolver<TGraphType>.FindCycleFrom(_node,out visitedNodes)) {
+                    if(CycleSolver<TGraphType>.FindCycleFrom(_node, out visitedNodes)) {
                         //? we found a cycle 
-                    }
-                }                
+                        return false;
+                    } // ? visitedNodes should now contain every node in the tree
+                } else {
+                    //? this graph has a node that is not reachable from the first node
+                    //? since we didn't find a cycle in the its connected component we visited all of it 
+                    //? therefore the graph is not connected and not a tree
+                    return false;
+                }               
             }
             return true;
         }
