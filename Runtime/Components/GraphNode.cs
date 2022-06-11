cfbf32dadabc1a74e9b998e937dd3424e382e646
cmwedin <cmwedin@gmail.com>
2022-06-11 14:13:44 -0400

    GraphNode change implementation

--- Runtime/Components/GraphNode.cs
+++ Runtime/Components/GraphNode.cs
@@ -17,28 +17,12 @@ namespace SadSapphicGames.CustomGraphs
 // * Reference Types - Public
         public TGraphType Value { get => value;}
         public AbstractGraph<TGraphType> ParentGraph { get => parentGraph;}
-        public List<string> EdgeIDs { get { // //? since the list is of value types modifications to this have no effect on the actual edges
-                List<string> output = new List<string>();
-                foreach (var id in inEdgeIDs) {
-                    output.Add(id);
-                }
-                foreach (var id in outEdgeIDs) {
-                    if(!output.Contains(id))output.Add(id);
-                }
-                return output;
-        }}
-        public List<int> NeighborIDs { get { 
-            List<int> output = new List<int>();
-            foreach (var edge in GetOutEdges()) {
-                output.Add(edge.GetOppositeNode(this).ID);
-            }
-            return output;
-        } }
 
 
-// * Reference Types - Private
+
+        // * Reference Types - Private
         private TGraphType value; // ? This could hypothetically be a value type
-        private AbstractGraph<TGraphType> parentGraph;
+        private AbstractGraph<TGraphType> parentGraph = null;
         private List<string> outEdgeIDs = new List<string>();
         private List<string> inEdgeIDs = new List<string>(); // ? undirected edges are both inEdges and 
 
@@ -54,15 +38,33 @@ namespace SadSapphicGames.CustomGraphs
             List<AbstractEdge<TGraphType>> output = ParentGraph.GetEdgeList(inEdgeIDs);
             return output.AsReadOnly();
         }
+        public List<string> GetEdgeIDs() { // //? since the list is of value types modifications to this have no effect on the actual edges
+            List<string> output = new List<string>();
+            foreach (var id in inEdgeIDs)
+            {
+                output.Add(id);
+            }
+            foreach (var id in outEdgeIDs)
+            {
+                if (!output.Contains(id)) output.Add(id);
+            }
+            return output;
+        }
+        public List<int> GetNeighborIDs() {
+            List<int> output = new List<int>();
+            foreach (var edge in GetOutEdges())
+            {
+                output.Add(edge.GetOppositeNode(this).ID);
+            }
+            return output;
+        }
 
 // * Constructors
         public GraphNode(
             int _id,
-            AbstractGraph<TGraphType> _parentGraph = null,
             TGraphType value = default(TGraphType)
         ) {
             this.id = _id;
-            this.parentGraph = _parentGraph;
             this.value = value;
         }
 
@@ -80,6 +82,10 @@ namespace SadSapphicGames.CustomGraphs
             value = _value;
         }
         internal void AddEdge(AbstractEdge<TGraphType> _edge) {
+            if(parentGraph == null) {
+                Debug.LogWarning("This node must be added to a graph before it can have edges added");
+                return;
+            }
             if(outEdgeIDs.Contains(_edge.ID) || inEdgeIDs.Contains(_edge.ID)) return;
             if(_edge.SinkNodeID == this.ID) { inEdgeIDs.Add(_edge.ID);} //? if this node is a sink add it to in edges
             if(_edge.GetOppositeNode(this) != this){outEdgeIDs.Add(_edge.ID);} //? if the other node is accessible add it to out edge (and undirected edge will be both) 
@@ -87,10 +93,18 @@ namespace SadSapphicGames.CustomGraphs
         internal void RemoveEdge(AbstractEdge<TGraphType> edge) {
             //? this should only be called from AbstractGraph.RemoveEdge(edge)
             //? which will make sure both nodes of an edge have it removed
+            if(parentGraph == null) {
+                Debug.LogWarning("This node must be added to a graph before it can have edges removed");
+                return;
+            }
             inEdgeIDs.Remove(edge.ID);
             outEdgeIDs.Remove(edge.ID);
         }
         internal void ClearEdges() { // ? this should only be used when moving a node from one graph to another;
+            if(parentGraph != null) {
+                Debug.LogWarning("This node must be orphaned before having its edges cleared");
+                return;
+            }
             inEdgeIDs = new List<string>();
             outEdgeIDs = new List<string>();
         }
