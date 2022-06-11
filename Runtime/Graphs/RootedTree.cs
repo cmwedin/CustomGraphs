79af2836d99c5b8a653374fa90d15e7177618a31
cmwedin <cmwedin@gmail.com>
2022-06-11 15:00:23 -0400

    Rooted/Tree change Implementation

--- Runtime/Graphs/RootedTree.cs
+++ Runtime/Graphs/RootedTree.cs
@@ -7,17 +7,19 @@ using UnityEngine;
 // ? 
 namespace SadSapphicGames.CustomGraphs {
     public class RootedTree<TGraphType> : Tree<TGraphType> {
-        public GraphNode<TGraphType> RootNode { get {
+        public GraphNode<TGraphType> FindRootNode()
+        {
             // ! this is probably not efficient
             // ! linear worst case (tree is a linked list)
             // ! should set a reference to this so we can get the root in constant time but would need to update that when operations that change the root happen
             var node = GetRandomNode();
-            while(node.GetInEdges().Count != 0) {
-                node = node.GetInEdges()[0].GetOppositeNode(node); 
+            while (node.GetInEdges().Count != 0)
+            {
+                node = node.GetInEdges()[0].GetOppositeNode(node);
             }
             return node;
-        }}
-        public GraphNode<TGraphType> GetParentNode(GraphNode<TGraphType> node) {
+        }
+        public GraphNode<TGraphType> GetParentNodeOf(GraphNode<TGraphType> node) {
             if (node.ParentGraph != this) { throw new DifferentGraphsException();}
             if (node.GetInEdges().Count == 0) {
                 Debug.LogWarning("This is the root node, returning null for its parent");
@@ -27,7 +29,7 @@ namespace SadSapphicGames.CustomGraphs {
             }
         }
         public List<GraphNode<TGraphType>> GetLayer(int k) {
-            List<GraphNode<TGraphType>> prevLayer = new List<GraphNode<TGraphType>>{RootNode};
+            List<GraphNode<TGraphType>> prevLayer = new List<GraphNode<TGraphType>>{ FindRootNode() };
             IEnumerable<GraphNode<TGraphType>> nextLayer;
 
             int currentDepth = 0;
@@ -60,37 +62,37 @@ namespace SadSapphicGames.CustomGraphs {
         public RootedTree() : base() {
         }
 // * Overrides
-        public override bool TryAddEdge(int id1, int id2) {
-            if(nodes.ContainsKey(id1)) {
-                if(nodes.ContainsKey(id2)) {
-                    Debug.LogWarning("Trees cannot have an edge added between two existing nodes without creating a cycle, which trees do not contain");
-                    return false;
-                } else {
-                    return TryAddEdge(GetNode(id1), new GraphNode<TGraphType>(id2));
-                }
-            } else if(nodes.ContainsKey(id2)) {
-                Debug.LogWarning($"you are trying to add a new in edge to node {id2}. A node in a rooted tree can only have one parent / in edge");
-                return false;
-            } else {
-                //? neither node is a part of the tree
-                Debug.LogWarning("at least one node of the edge must already be a part of the tree as a tree only contains one connected component");
-                return false;
-            }          
-        }
-        public override bool TryAddEdge(GraphNode<TGraphType> v1, GraphNode<TGraphType> v2) {
-            if(v1.ParentGraph == null && v2.ParentGraph == this) {
-                Debug.LogWarning($"you are trying to add a new in edge to node {v2.ID}. A node in a rooted tree can only have one parent / in edge");
-                return false;
-            } else if(v1.ParentGraph == this && v2.ParentGraph == null) {
-                AddNode(v2); //TODO don't like this - see tree class
-                var _edge = new UndirectedEdge<TGraphType>(v1,v2);
-                edges.Add(_edge.ID,_edge);
-                return true;
-            } else {
-                Debug.LogWarning("An edge can only be added to a tree between a node it already contains and a new/orphan node, doing otherwise would break the tree condition");
-                return false;
-            }
-        }
+        // public override bool TryAddEdge(int id1, int id2) {
+        //     if(nodes.ContainsKey(id1)) {
+        //         if(nodes.ContainsKey(id2)) {
+        //             Debug.LogWarning("Trees cannot have an edge added between two existing nodes without creating a cycle, which trees do not contain");
+        //             return false;
+        //         } else {
+        //             return TryAddEdge(GetNode(id1), new GraphNode<TGraphType>(id2));
+        //         }
+        //     } else if(nodes.ContainsKey(id2)) {
+        //         Debug.LogWarning($"you are trying to add a new in edge to node {id2}. A node in a rooted tree can only have one parent / in edge");
+        //         return false;
+        //     } else {
+        //         //? neither node is a part of the tree
+        //         Debug.LogWarning("at least one node of the edge must already be a part of the tree as a tree only contains one connected component");
+        //         return false;
+        //     }          
+        // }
+        // public override bool TryAddEdge(GraphNode<TGraphType> v1, GraphNode<TGraphType> v2) {
+        //     if(v1.ParentGraph == null && v2.ParentGraph == this) {
+        //         Debug.LogWarning($"you are trying to add a new in edge to node {v2.ID}. A node in a rooted tree can only have one parent / in edge");
+        //         return false;
+        //     } else if(v1.ParentGraph == this && v2.ParentGraph == null) {
+        //         TryAddNode(v2); //TODO don't like this - see tree class
+        //         var _edge = new UndirectedEdge<TGraphType>(v1.ID,v2.ID);
+        //         edges.Add(_edge.ID,_edge);
+        //         return true;
+        //     } else {
+        //         Debug.LogWarning("An edge can only be added to a tree between a node it already contains and a new/orphan node, doing otherwise would break the tree condition");
+        //         return false;
+        //     }
+        // }
         public override bool TryAddEdge(AbstractEdge<TGraphType> edge) {
             if(!(edge is UndirectedEdge<TGraphType>)) {
                 Debug.LogWarning("A Tree is a type of undirected graph");
@@ -107,7 +109,7 @@ namespace SadSapphicGames.CustomGraphs {
                     Debug.LogWarning($"you are trying to add a new in edge to node {edge.SinkNodeID}. A node in a rooted tree can only have one parent / in edge");
                     return false;
                 } else if(nodes.ContainsKey(edge.SourceNodeID) && !nodes.ContainsKey(edge.SinkNodeID)) { 
-                    AddNode(edge.SinkNodeID); 
+                    TryAddNode(new GraphNode<TGraphType>(edge.SinkNodeID)); 
                     edge.SetParent(this);
                     GetNode(edge.SourceNodeID).AddEdge(edge);
                     GetNode(edge.SinkNodeID).AddEdge(edge);
--- Runtime/Graphs/Tree.cs
+++ Runtime/Graphs/Tree.cs
@@ -4,40 +4,56 @@ using UnityEngine;
 namespace SadSapphicGames.CustomGraphs {
     public class Tree<TGraphType> : UndirectedGraph<TGraphType>
     {
-// * Overrides
-        public override bool TryAddEdge(int id1, int id2) {
-            if(nodes.ContainsKey(id1)) {
-                if(nodes.ContainsKey(id2)) {
-                    Debug.LogWarning("Trees cannot have an edge added between two existing nodes without creating a cycle, which trees do not contain");
-                    return false;
-                } else {
-                    return TryAddEdge(GetNode(id1), new GraphNode<TGraphType>(id2));
-                }
-            } else if(nodes.ContainsKey(id2)) {
-                //? we already know id doesnt contain id1
-                return TryAddEdge(new GraphNode<TGraphType>(id1), GetNode(id2));
-            } else {
-                //? neither node is a part of the tree
-                Debug.LogWarning("at least one node of the edge must already be a part of the tree as a tree only contains one connected component");
-                return false;
-            }          
-        }
-        public override bool TryAddEdge(GraphNode<TGraphType> v1, GraphNode<TGraphType> v2) {
-            if(v1.ParentGraph == null && v2.ParentGraph == this) {
-                AddNode(v1); //TODO I don't like being able to do this, adding a node without adding an edge as well would break the tree condition
-                var _edge = new UndirectedEdge<TGraphType>(v1,v2);
-                edges.Add(_edge.ID,_edge);
-                return true;
-            } else if(v1.ParentGraph == this && v2.ParentGraph == null) {
-                AddNode(v2); //TODO " "
-                var _edge = new UndirectedEdge<TGraphType>(v1,v2);
-                edges.Add(_edge.ID,_edge);
-                return true;
-            } else {
-                Debug.LogWarning("An edge can only be added to a tree between a node it already contains and a new/orphan node, doing otherwise would break the tree condition");
+        // * Overrides
+        public override bool TryAddNode(GraphNode<TGraphType> node)
+        {
+            if(Size != 0) {
+                Debug.LogWarning("You can only add an individual node to an empty tree, for all subsequent nodes they must be added through TryAddEdge with one new node id");
                 return false;
             }
+            return base.TryAddNode(node);
         }
+        public override bool TryRemoveNode(GraphNode<TGraphType> node)
+        {
+            if(node.GetOutEdges().Count != node.GetInEdges().Count) {
+                Debug.LogWarning("only leaf nodes can be removed without breaking the tree condition");
+                return false;
+            }
+            return base.TryRemoveNode(node);
+        }
+        // public override bool TryAddEdge(int id1, int id2) {
+        //     if(nodes.ContainsKey(id1)) {
+        //         if(nodes.ContainsKey(id2)) {
+        //             Debug.LogWarning("Trees cannot have an edge added between two existing nodes without creating a cycle, which trees do not contain");
+        //             return false;
+        //         } else {
+        //             return TryAddEdge(GetNode(id1), new GraphNode<TGraphType>(id2));
+        //         }
+        //     } else if(nodes.ContainsKey(id2)) {
+        //         //? we already know id doesnt contain id1
+        //         return TryAddEdge(new GraphNode<TGraphType>(id1), GetNode(id2));
+        //     } else {
+        //         //? neither node is a part of the tree
+        //         Debug.LogWarning("at least one node of the edge must already be a part of the tree as a tree only contains one connected component");
+        //         return false;
+        //     }          
+        // }
+        // public override bool TryAddEdge(GraphNode<TGraphType> v1, GraphNode<TGraphType> v2) {
+        //     if(v1.ParentGraph == null && v2.ParentGraph == this) {
+        //         TryAddNode(v1); //TODO I don't like being able to do this, adding a node without adding an edge as well would break the tree condition
+        //         var _edge = new UndirectedEdge<TGraphType>(v1,v2);
+        //         edges.Add(_edge.ID,_edge);
+        //         return true;
+        //     } else if(v1.ParentGraph == this && v2.ParentGraph == null) {
+        //         TryAddNode(v2); //TODO " "
+        //         var _edge = new UndirectedEdge<TGraphType>(v1,v2);
+        //         edges.Add(_edge.ID,_edge);
+        //         return true;
+        //     } else {
+        //         Debug.LogWarning("An edge can only be added to a tree between a node it already contains and a new/orphan node, doing otherwise would break the tree condition");
+        //         return false;
+        //     }
+        // }
         public override bool TryAddEdge(AbstractEdge<TGraphType> edge) {
             if(!(edge is UndirectedEdge<TGraphType>)) {
                 Debug.LogWarning("A Tree is a type of undirected graph");
@@ -51,14 +67,14 @@ namespace SadSapphicGames.CustomGraphs {
                 return false;
             } else {
                 if(!nodes.ContainsKey(edge.SourceNodeID) && nodes.ContainsKey(edge.SinkNodeID)) { 
-                    AddNode(edge.SourceNodeID); 
+                    TryAddNode(new GraphNode<TGraphType>(edge.SourceNodeID)); 
                     edge.SetParent(this);
                     GetNode(edge.SourceNodeID).AddEdge(edge);
                     GetNode(edge.SinkNodeID).AddEdge(edge);
                     edges.Add(edge.ID, edge);
                     return true;
                 } else if(nodes.ContainsKey(edge.SourceNodeID) && !nodes.ContainsKey(edge.SinkNodeID)) { 
-                    AddNode(edge.SinkNodeID); 
+                    TryAddNode(new GraphNode<TGraphType>(edge.SinkNodeID)); 
                     edge.SetParent(this);
                     GetNode(edge.SourceNodeID).AddEdge(edge);
                     GetNode(edge.SinkNodeID).AddEdge(edge);
@@ -69,6 +85,10 @@ namespace SadSapphicGames.CustomGraphs {
                     return false;
                 }
             }
+        }
+        public override bool TryRemoveEdge(AbstractEdge<TGraphType> edge) {
+            Debug.LogWarning("edges cannot be removed from trees without breaking the tree condition. If you are trying to remove a leaf use TryRemoveNode. If you are trying to replace this edge with a new one, use TryReplaceEdge");
+            return false;
         }
         public override bool TryReplaceEdge(AbstractEdge<TGraphType> oldEdge, AbstractEdge<TGraphType> newEdge) {
            if(newEdge.GetType() != oldEdge.GetType()) {
